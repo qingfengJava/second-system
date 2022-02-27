9c14a978581e8678354ea9f4caafe5c599bb1303
pengjialong <2562777581@qq.com>
2022-02-27 23:46:35 +0800

    活动申请报名查询等功能

---
+++ second-class-development-system/mapper/src/main/java/com/qingfeng/dao/NoticeMapper.java
@@ -0,0 +1,7 @@
+package com.qingfeng.dao;
+
+import com.qingfeng.entity.Notice;
+import com.qingfeng.generaldao.GeneralDao;
+
+public interface NoticeMapper extends GeneralDao<Notice> {
+}
---
+++ second-class-development-system/mapper/src/main/java/com/qingfeng/dao/OrganizeImgMapper.java
@@ -0,0 +1,7 @@
+package com.qingfeng.dao;
+
+import com.qingfeng.entity.OrganizeImg;
+import com.qingfeng.generaldao.GeneralDao;
+
+public interface OrganizeImgMapper extends GeneralDao<OrganizeImg> {
+}
