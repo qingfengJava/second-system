f35247958ba0f083767618993368a0892f27ca5f
qingfeng <2562777581@qq.com>
2022-05-11 01:27:54 +0800

    用户基本信息管理

---
+++ second-class-development-system/mapper/src/main/java/com/qingfeng/dao/InfoMaintainMapper.java
@@ -0,0 +1,12 @@
+package com.qingfeng.dao;
+
+import com.qingfeng.entity.InfoMaintain;
+import com.qingfeng.generaldao.GeneralDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author 清风学Java
+ */
+@Repository
+public interface InfoMaintainMapper extends GeneralDao<InfoMaintain> {
+}
